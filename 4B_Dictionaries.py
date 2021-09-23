395c36c6091d6475eece39576a56b7287de07e38
Arina Apetyan <aaapetyan@gmail.com>
2021-09-23 19:35:24 +0300

    Update 4B_Dictionaries.py
    added problem D

--- 4B_Dictionaries.py
+++ 4B_Dictionaries.py
@@ -42,3 +42,41 @@ sort_by_count = sorted(sort_by_word, key = lambda words: words[1],reverse=True)
 for words in sort_by_count:
     print(words[0])
 
+# https://contest.yandex.ru/contest/28970/problems/D/
+
+all_votes = []
+sum_votes = 0
+remaining_seats = 450
+
+i = 0
+with open('input.txt','r') as fin:
+    for line in fin.readlines():
+        party, votes = line.rsplit(' ',1)
+        all_votes.append([i, party, int(votes)])
+        sum_votes += int(votes)
+        i += 1
+
+for i in range(len(all_votes)):
+    temp = all_votes[i][2] / sum_votes * 450.0
+    all_votes[i][2] = [int(temp),temp-int(temp)]
+    remaining_seats -= int(temp)
+    
+all_votes = sorted(all_votes, key = lambda votes: votes[2][1], reverse = True)
+
+i = 0
+
+while remaining_seats > 0:
+    all_votes[i][2][0] += 1
+    remaining_seats -= 1
+    i += 1
+    if i == len(all_votes):
+        i = 0
+        
+all_votes = sorted(all_votes, key = lambda votes: votes[0])        
+
+print_line = ''
+
+for i in range(len(all_votes)):
+    print_line += all_votes[i][1] + ' ' + str(all_votes[i][2][0]) + '\n'
+    
+print(print_line)
